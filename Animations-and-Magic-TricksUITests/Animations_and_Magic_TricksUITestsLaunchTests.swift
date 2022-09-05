4c02e0db39b86117641a3be5032ec56d9c02fdeb
Ilya Cherkasov <cherkasov-i@surf.dev>
2022-09-05 20:26:59 +0300

    Initial commit

---
+++ Animations-and-Magic-TricksUITests/Animations_and_Magic_TricksUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Animations_and_Magic_TricksUITestsLaunchTests.swift
+//  Animations-and-Magic-TricksUITests
+//
+//  Created by Ilya Cherkasov on 05.09.2022.
+//
+
+import XCTest
+
+class Animations_and_Magic_TricksUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
